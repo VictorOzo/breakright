4fdc7d82bc0549078e0b7884d4286c054bb54013
Ozokwelu Victor <ozokweluvic@gmail.com>
2024-10-29 14:08:49 -0700

    catalog section created

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,4 +1,10 @@
 import "./home.css";
+import cardimg1 from "../../assets/images/cardimg1.png";
+import cardimg2 from "../../assets/images/cardimg2.png";
+import cardimg3 from "../../assets/images/cardimg3.png";
+import cardimg4 from "../../assets/images/cardimg4.png";
+import cardimg5 from "../../assets/images/cardimg5.png";
+import cardimg6 from "../../assets/images/cardimg6.png";
 
 export const Home = () => {
   return (
@@ -25,9 +31,34 @@ export const Home = () => {
           <p>Discover Our Product Categories</p>
           <h2>Built for performance, Designed for durability</h2>
         </div>
-        <div className="catalog_card">
-          <div className="card">
-            <img src="" alt="" />
+        <div className="catalog_card grid grid-cols-3 gap-5">
+          <div className="card w-[400px] h-[340px] rounded-[15px] bg-[blue] ">
+            <img src={cardimg1} />
+            <h2>Title</h2>
+            <p>Value</p>
+          </div>
+          <div className="card w-[400px] h-[340px] rounded-[15px] bg-[blue] ">
+            <img src={cardimg2} />
+            <h2>Title</h2>
+            <p>Value</p>
+          </div>
+          <div className="card w-[400px] h-[340px] rounded-[15px] bg-[blue] ">
+            <img src={cardimg3} />
+            <h2>Title</h2>
+            <p>Value</p>
+          </div>
+          <div className="card w-[400px] h-[340px] rounded-[15px] bg-[blue] ">
+            <img src={cardimg4} />
+            <h2>Title</h2>
+            <p>Value</p>
+          </div>
+          <div className="card w-[400px] h-[340px] rounded-[15px] bg-[blue] ">
+            <img src={cardimg5} />
+            <h2>Title</h2>
+            <p>Value</p>
+          </div>
+          <div className="card w-[400px] h-[340px] rounded-[15px] bg-[blue] ">
+            <img src={cardimg6} />
             <h2>Title</h2>
             <p>Value</p>
           </div>
